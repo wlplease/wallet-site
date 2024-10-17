37407d12c68c05e74699bd54f8cbdc892d9082bc
Varun Singh <varunsingh9114@gmail.com>
2024-10-17 18:55:36 +0530

    home: Add option to copy wallet address to clipboard.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,16 +10,21 @@ import {
     CardTitle
 }
 from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SendSol from "./send_sol";
 import sol from "@/public/solanaLogoMark.png";
+import { ClipboardCopyIcon } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
 
 export default function Home(){
     const wallet = useWallet();
     const { connection } = useConnection();
     const [balance,setBalance] = useState<number>(0);
+    const pub_key_ref = useRef<HTMLParagraphElement>(null);
+    const { toast } = useToast();
 
     useEffect(()=>{
         async function get_balance(){
@@ -31,9 +36,26 @@ export default function Home(){
         }
 
         get_balance();
-    },[wallet.publicKey])
+    },[wallet.publicKey,connection])
 
-    
+    function copy_to_clipboard(){
+        const pub_key_node = pub_key_ref.current;
+
+        if(pub_key_node !== null){
+            const pub_key_elem = pub_key_node.querySelector("#public_key");
+            if(pub_key_elem !== null){
+                navigator.clipboard.writeText(pub_key_elem.textContent ?? "").then(
+                    () => {
+                        toast({
+                            title: "Copied to Clipboard!",
+                            duration: 2000,
+                        })
+                    }
+                )
+            }
+        }
+    }
+
     return (
         <div className="flex flex-col items-center space-y-12 text-center">
             {wallet.publicKey ? <Card className="">
@@ -43,7 +65,19 @@ export default function Home(){
                 </CardHeader>
                 <CardContent className="">
                     <h2 className="text-3xl">{balance.toString()} SOL</h2>
-                    <p className="text-muted-foreground text-nowrap bg-zinc-200 dark:bg-zinc-600 p-1 mt-2">{wallet.publicKey?.toBase58()}</p>
+                    <div
+                    ref={pub_key_ref}
+                    className="flex w-full bg-zinc-200 dark:bg-zinc-600 rounded-md">
+                    <p
+                    id="public_key"
+                    className="text-muted-foreground text-nowrap p-2 m-1">
+                        {wallet.publicKey?.toBase58()}
+                    </p>
+                    <Button 
+                    onClick={copy_to_clipboard}
+                    className="m-2"
+                    variant={"secondary"} size={"icon"}><ClipboardCopyIcon className=""/></Button>
+                    </div>
                     <div className="flex justify-between mt-4">
                         <SendSol max_bal={balance}/>
                     </div>
